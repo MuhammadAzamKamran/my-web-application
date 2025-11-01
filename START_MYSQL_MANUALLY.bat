7f851113d7a8f68a405fe415a554241da7ca9af7
MuhammadAzamKamran <164350812+MuhammadAzamKamran@users.noreply.github.com>
2025-11-01 22:39:34 +0000

    Delete START_MYSQL_MANUALLY.bat

--- START_MYSQL_MANUALLY.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /D C:\xampp
-echo Starting MySQL manually...
-echo This window must stay open while MySQL is running
-echo To stop MySQL, press Ctrl+C in this window
-echo.
-mysql\bin\mysqld --defaults-file=mysql\bin\my.ini --standalone
-
